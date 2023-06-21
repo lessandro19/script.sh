c8f0d5550906f858d6a113be835ef05a2919ac7b
lessandro19 <111545061+lessandro19@users.noreply.github.com>
2023-06-20 21:35:04 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+clear
+echo "Nome completo: alessandro rezende manoel da silva "
+echo "Nome do usuário: alessandro"
+echo "As últimas 6 linhas do arquivo de configuração do gerenciador de pacotes APT são:"
+tail -n 6 /etc/apt/apt.conf
